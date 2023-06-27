8fd713b69535acfce87268c499a8aba689767b7b
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-27 05:16:53 +0300

    Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    # Initializing the session[:page_views] to 0 if it doesn't exist
+    session[:page_views] ||= 0
+
+    # Incrementing the session[:page_views] by 1 for each request
+    session[:page_views] += 1
+
+    # Finding the requested article
     article = Article.find(params[:id])
-    render json: article
+
+    # If the user has viewed fewer than 3 pages, render a JSON response with the article data
+    if session[:page_views] <= 3
+      render json: article
+    else
+      # If the user has viewed 3 or more pages, render a JSON response with an error message and a status code of 401 unauthorized
+      render json: { error: 'Paywall activated. Subscribe for unlimited access.' }, status: :unauthorized
+    end
   end
 
   private
